850e2dfcc817b97ac711b2e57128eea27ae7bb85
peter014 <pipeyodavid@hotmail.com>
2016-11-03 23:00:24 -0500

    ultimos cambios

--- app/src/main/java/com/example/ceaiot/qrreader/MainActivity.java
+++ app/src/main/java/com/example/ceaiot/qrreader/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
 
@@ -23,9 +25,13 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     private static Socket s;
     private static PrintWriter printWriter;
 
+    String ipReg = "";
+    String ipDem = "";
+    String ip = "";
+    int puerto = 5000;
     private String message = "";
-    private static String ip = "192.168.0.11";
-
+    private static final String IPADDRESS_PATTERN ="^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+            "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,12 +78,45 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         scanner.startCamera();
     }
 
+    public void onClickEnviarRegistro(View view){
+        TextView textView = (TextView) findViewById(R.id.ipRegistro);
+        String s = textView.getText().toString();
+        if(s.matches(IPADDRESS_PATTERN)){
+            ip = s;
+            ipReg = s;
+            puerto = 5000;
+            sendMessage();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"IP Registro incorrecta",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onClickEnviarDemo(View view){
+        TextView textView = (TextView) findViewById(R.id.ipDemo);
+        String s = (String) textView.getText().toString();
+        if(s.matches(IPADDRESS_PATTERN)){
+            ip = s;
+            ipDem = s;
+            puerto = 5001;
+            sendMessage();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"IP Demo incorrecta",Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     public void onBackPressed() {
         if (scanner != null && scanner.isEnabled()) {
             setContentView(R.layout.activity_main);
             scanner.stopCamera();
             scanner = null;
+            EditText t = (EditText) findViewById(R.id.ipDemo);
+            t.setText(ipDem);
+
+            EditText te = (EditText) findViewById(R.id.ipRegistro);
+            te.setText(ipReg);
         }
         else{
             this.finish();
@@ -94,7 +133,7 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         @Override
         protected Void doInBackground(Void... params) {
             try {
-                s = new Socket(ip,5000);
+                s = new Socket(ip,puerto);
                 printWriter = new PrintWriter(s.getOutputStream());
                 printWriter.write(message);
                 printWriter.flush();
@@ -117,7 +156,13 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
 
         TextView textView = (TextView) findViewById(R.id.result);
         message  = result.getText();
-        textView.setText("Correo: "+message);
-        sendMessage();
+        textView.setText("Correo: " + message);
+
+        EditText t = (EditText) findViewById(R.id.ipDemo);
+        t.setText(ipDem);
+
+        EditText te = (EditText) findViewById(R.id.ipRegistro);
+        te.setText(ipReg);
+
     }
 }
